353a317f94e9a17faeb7c9b31c0e595a2ebdeed0
Jonathan Lyon <jessicacrawfordoptimus010@gmail.com>
2022-04-24 18:44:56 -0400

    Initial commit

---
+++ Main.js
@@ -0,0 +1,10 @@
+import Nethereum.HdWallet;
+
+string = "radar blur sun chef fix engine embark joy scheme fiction master release";
+wallet = new Wallet(words, null);
+Console.WriteLine(Wallet.GetAccount(0).Address)
+
+// wallet = new Wallet(Wordlist.English, WordCount.Twelve);
+
+// Console.WriteLine("Addresses:");
+// for(int i=0, i<20, i++)
