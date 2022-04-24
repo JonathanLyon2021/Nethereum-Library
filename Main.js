a1c85ce99359a352feb6c161821771515789825c
Jonathan Lyon <jessicacrawfordoptimus010@gmail.com>
2022-04-24 19:15:22 -0400

    Create a random HD wallet

--- Main.js
+++ Main.js
@@ -1,10 +1,27 @@
-import Nethereum.HdWallet;
+//THIS EXERCISE IS IN C# LANGUAGE 
 
-string = "radar blur sun chef fix engine embark joy scheme fiction master release";
-wallet = new Wallet(words, null);
-Console.WriteLine(Wallet.GetAccount(0).Address)
+// import Nethereum.HdWallet;
+
+// string words = "radar blur sun chef fix engine embark joy scheme fiction master release";
+// Wallet wallet = new Wallet(words, null);
+// Console.WriteLine(Wallet.GetAccount(0).Address);
 
 // wallet = new Wallet(Wordlist.English, WordCount.Twelve);
 
 // Console.WriteLine("Addresses:");
-// for(int i=0, i<20, i++)
+// for(int i=0,
+
+
+
+//Example: create a new random HD wallet
+
+Wallet wallet = 
+    new Wallet(Wordlist.English, WordCount.Twelve);
+
+    //Example: derive addresses and print them on the console.
+
+Console.WriteLine("Addresses:");
+for(int i = 0; i < 20; i++)
+{
+    Console.WriteLine(wallet.GetAccount(i).Address)
+}
